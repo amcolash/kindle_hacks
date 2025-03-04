30234913d7db5523d6cd722e28f218bb24f5e9b5
Andrew McOlash <amcolash@gmail.com>
2025-03-04 13:10:50 +0400

    add kual menu entry and fix some bugs

--- page_turn/src/page_turn.py
+++ page_turn/src/page_turn.py
@@ -1,6 +1,15 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 import subprocess
 import sys
+import signal
+
+# Get absolute directory of this script
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+INDEX_PATH = os.path.join(SCRIPT_DIR, "index.html")
+NEXT_PAGE_PATH = os.path.join(SCRIPT_DIR, "next_page")
+PREVIOUS_PAGE_PATH = os.path.join(SCRIPT_DIR, "previous_page")
 
 class RequestHandler(BaseHTTPRequestHandler):
   def do_GET(self):
@@ -13,7 +22,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 
   def serve_index(self):
     try:
-      with open("index.html", "rb") as file:
+      with open(INDEX_PATH, "rb") as file:
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
@@ -27,9 +36,9 @@ class RequestHandler(BaseHTTPRequestHandler):
     print("POST request received", self.path)
 
     if self.path == "/next":
-      self.run_command("evemu-play /dev/input/event1 < ./next_page")
+      self.run_command(f"evemu-play /dev/input/event1 < {NEXT_PAGE_PATH}")
     elif self.path == "/previous":
-      self.run_command("evemu-play /dev/input/event1 < ./previous_page")
+      self.run_command(f"evemu-play /dev/input/event1 < {PREVIOUS_PAGE_PATH}")
     elif self.path == "/stop":
       self.send_response(200)
       self.end_headers()
@@ -55,24 +64,35 @@ def setup():
   # Open port 8080 in iptables
   command = "/usr/sbin/iptables -A INPUT -i wlan0 -p tcp --dport 8080 -j ACCEPT"
   subprocess.run(command, shell=True, capture_output=True, text=True)
+
   print("Opened port 8080")
 
-def cleanup():
+def shutdown_server():
   # Close port 8080 in iptables
   command = "/usr/sbin/iptables -D INPUT -i wlan0 -p tcp --dport 8080 -j ACCEPT"
   subprocess.run(command, shell=True, capture_output=True, text=True)
   print("Closed port 8080")
 
+  print("Shutting down server...")
+  httpd.server_close()
+  sys.exit(0)
+
+# Handle SIGINT (Ctrl+C) and SIGTERM (kill)
+def signal_handler(signum, frame):
+  shutdown_server()
+
 if __name__ == "__main__":
-  setup()
+  setup()  # Open port on start
 
   server_address = ("", 8080)
   httpd = HTTPServer(server_address, RequestHandler)
   print("Server running on port 8080...")
 
+  # Register signal handlers
+  signal.signal(signal.SIGINT, signal_handler)
+  signal.signal(signal.SIGTERM, signal_handler)
+
   try:
     httpd.serve_forever()
   except KeyboardInterrupt:
-    print("Shutting down server...")
-    cleanup()
-    httpd.server_close()
+    shutdown_server()
